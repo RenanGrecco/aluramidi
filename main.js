17ff183258345e520008166b350c3ecf4e6b21f7
RenanGrecco <127275458+RenanGrecco@users.noreply.github.com>
2023-03-09 13:18:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+function tocaSom (seletorAudio){
+
+    const elemento = document.querySelector(seletorAudio); 
+
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+        }
+    else {
+        console.log ('Elemento não encontrado ou seletor inválido')
+        }
+
+    }
+    
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+//listaDeTeclas.lenght = 9
+//antes foi usado while
+//para 
+
+for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    //console.log (instrumento)
+    
+    const idAudio = `#som_${instrumento}`   //template string
+    
+    //console.log(idAudio);
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+
+        if (evento.code === 'Space' || evento.code === 'Enter' || evento.code === 'NumpadEnter' ) {
+            tecla.classList.add('ativa');
+        }
+        
+        
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
+
